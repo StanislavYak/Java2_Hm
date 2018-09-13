96712d039c861c47239b2d3e0aab12c0bd489c83
Stas <karueli59@gmail.com>
2018-09-13 13:59:39 +0300

    Hm7

---
+++ hm7/AuthService.java
@@ -0,0 +1,9 @@
+package ru.hm;
+
+public interface AuthService {
+    String getNick(String login, String pass);
+
+    boolean login(String login, String pass);
+
+    boolean contains(String userName);
+}
